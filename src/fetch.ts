bbabc4fda05eb7ccdf0ddb128f56b6f05f264fa6
Dooy <dooy520@qq.com>
2023-08-11 17:42:16 +0800

    auth

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,6 +2,7 @@ import { fetch, setGlobalDispatcher, ProxyAgent } from 'undici';
 
 const httpProxy = process.env.http_proxy || process.env.HTTP_PROXY;
 if (httpProxy) {
+  console.log("httpProxy",httpProxy )
   setGlobalDispatcher(new ProxyAgent(httpProxy));
 }
 
